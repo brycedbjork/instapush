8ca1b94b95522e04330b9f3278f7712f687b41e3
Bryce Bjork <brycedbjork@gmail.com>
2023-04-04 11:11:45 -0400

    - The instapush.sh file has been modified

--- instapush.sh
+++ instapush.sh
@@ -7,7 +7,7 @@ cd "$current_dir"
 git add .
 git_diff_summary="$(git diff --staged --stat)"
 git_diff_changes="$(git diff --staged --unified=0)"
-prompt="Describe these code changes simply and concisely in bullets separated by semicolons:\nSummary:\n$git_diff_summary\nChanges:\n$git_diff_changes"
+prompt="Describe the impact of these code changes simply and concisely in bullets separated by semicolons:\nSummary:\n$git_diff_summary\nChanges:\n$git_diff_changes"
 commit_msg=$(python "$script_dir/generate_commit_message.py" "$prompt")
 
 git commit -am "$commit_msg"
