e53802d5dd66865410764b8753220f592c2523fb
Bryce Bjork <brycedbjork@gmail.com>
2023-04-09 11:27:48 -0400

    Refactor the commit message for "optional argument":

--- instapush.sh
+++ instapush.sh
@@ -7,8 +7,16 @@ cd "$current_dir"
 git add .
 git_diff_summary="$(git diff --staged --stat)"
 git_diff_changes="$(git diff --staged --unified=0)"
-prompt="Create a message for the following:\nSummary:\n$git_diff_summary\nChanges:\n$git_diff_changes"
+
+if [ -z "$1" ]
+then
+    prompt="Create a message for the following:\nSummary:\n$git_diff_summary\nChanges:\n$git_diff_changes"
+else
+    prompt="Create a commit message for: $1\nSummary:\n$git_diff_summary\nChanges:\n$git_diff_changes"
+fi
+
 commit_msg=$(python "$script_dir/generate_commit_message.py" "$prompt")
 
 git commit -am "$commit_msg"
 git push
+
